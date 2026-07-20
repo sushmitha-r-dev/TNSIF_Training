a026dccb91f828d36bc31600345be7ab19acfe82
Sushmitha A R <Sushmitha A R@LAPTOP-F67KRURI>
2026-07-20 12:38:53 +0530

    First commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/javabasics/ReverseString.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/javabasics/ReverseString.java
@@ -10,7 +10,7 @@ public class ReverseString {
 		        for (int i = str.length() - 1; i >= 0; i--) {
 		            rev = rev + str.charAt(i);
 		        }
-
+		        System.out.println("Orginal String: " + str);
 		        System.out.println("Reversed String: " + rev);
 		    }
 		}
